2de294f971ff592fcd93e82f07663fc95724abfc
Declimator <31232768+Declimator@users.noreply.github.com>
2020-10-29 17:16:22 -0700

    BubbleSorter, SelectionSorter, InsertionSorter, QuickSorter IP

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -10,20 +10,16 @@ public class BubbleSorter extends Sorter{
 	//   progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		int min = array[0];
-		for(int i = 0; i < array.length-1; i++) {
-			if(array[i] < min) {
-				min = array[i];
-				for(int j = i; j > 0; j--) {
+		for(int i = 0; i < array.length; i++) {
+			for(int j = array.length-2; j >= 0; j--) {
+				if(array[j] > array[j+1]) {
 					int x = array[j];
-					array[j] = array[j-1];
-					display.updateDisplay();
+					array[j] = array[j+1];
+					array[j+1] = x;
+
 				}
-				array[0] = array[i];
-
-
 			}
-
+			display.updateDisplay();
 		}
 	}
 	
--- src/_00_Sorting_Algorithms/QuickSorter.java
+++ src/_00_Sorting_Algorithms/QuickSorter.java
@@ -18,12 +18,28 @@ public class QuickSorter extends Sorter {
 	private void quickSort(int[] array, int low, int high, SortingVisualizer display) {
 		//2. create two integer variables called i and j and set them equal
 		//   to low and high respectively.
-       
+       int i = low;
+       int j = high;
         //3. create an integer called pivot and set it equal to the element
         //   in the array that is the halfway point between low and high
-
+       int pivot = low + (high-low)/2;
         //4. make a while loop that goes while i is less than or equal to j
-        
+        while(i<=j) {
+        	while(array[i] < pivot) {
+        		i++;
+        	}
+        	while(array[j] > pivot) {
+        		j--;
+        	}
+        	if(i <= j) {
+        		int x = array[i];
+        		array[i] = array[j];
+        		array[j] = x;
+        		i++;
+        		j--;
+        	}
+        	
+        }
         	//5. make a while loop that increments i while 
         	//   the array element at i is less than pivot
             
