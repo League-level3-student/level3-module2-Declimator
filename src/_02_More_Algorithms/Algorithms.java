9b4576f54795fbb6466e70417fe8596a95368e3b
Declimator <31232768+Declimator@users.noreply.github.com>
2021-02-04 16:59:30 -0800

    MoreAndMoreTest, MoreAlgorithms IP

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,28 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for(int i = 0; i < eggs.size(); i++) {
+			if(eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int countPearls(List<Boolean> pearls) {
+		int counter = 0;
+		for(int i = 0; i < pearls.size(); i++) {
+			if(pearls.get(i) == true) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+	public static double findTallest(List<Double> peeps) {
+		double maximum = 0;
+		for(int i = 0; i < peeps.size(); i++) {
+			if(peeps.get(i) > maximum)
+				maximum = peeps.get(i);
+		}
+		return maximum;
 	}
 }
